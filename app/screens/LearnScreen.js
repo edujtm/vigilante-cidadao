f46b74764661e95c936decd27a24957f63d0f0a9
Eduardo José Tomé de Macedo <eduzemacedo@hotmail.com>
2019-07-20 10:37:02 -0300

    Adicionando marketing na LearnScreen

--- app/screens/LearnScreen.js
+++ app/screens/LearnScreen.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+import { Text } from 'react-native-paper';
+
+const LEARN_IMG = require('../../assets/learn.jpg');
 
 class LearnScreen extends Component {
   static navigationOptions = {
@@ -9,7 +12,11 @@ class LearnScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Learn Screen</Text>
+				<Image 
+					style={{ width: 240, height: 150 }}
+					source={LEARN_IMG}
+				/>
+        <Text style={styles.text}>Aprenda como fazer uma denúncia de forma efetiva</Text>
       </View>
     );
   }
@@ -20,7 +27,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+	text: {
+		marginTop: 20,
+		fontWeight: '600',
+		width: 300,
+		fontSize: 16,
+		textAlign: 'center',
+	}
 })
 
 export { LearnScreen };
